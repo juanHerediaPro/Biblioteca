e673cc02108e85e15186d84b665b974591ae018c
juanherediapro <juanherediau@gmail.com>
2025-10-28 08:53:14 -0500

    arreglo controller user, testeo

--- Biblioteca/src/main/java/com/biblioteca/Biblioteca/Controllers/UserController.java
+++ Biblioteca/src/main/java/com/biblioteca/Biblioteca/Controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.biblioteca.Biblioteca.Controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.biblioteca.Biblioteca.Entities.User;
 import com.biblioteca.Biblioteca.ServiceInterfaces.IUserServiceInterface;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -23,10 +30,18 @@ public class UserController {
     public ResponseEntity<Object> getUserByidCard(@PathVariable Long idCard){
         return iUserServiceInterface.getUserByIdCard(idCard);
     }
-    @GetMapping("/all")
+
+    @GetMapping({"", "/"}) // <--- AÑADE ESTA LÍNEA AQUÍ
     public ResponseEntity<Object> getAllUsers(){
         return iUserServiceInterface.getAllUsers();
     }
+    
+    // Dejar /all si quieres mantenerlo, o eliminar el mapeo si solo quieres el nuevo
+    @GetMapping("/all") 
+    public ResponseEntity<Object> getAllUsersDeprecated(){
+        return iUserServiceInterface.getAllUsers();
+    }
+    
     @DeleteMapping("/delete/{idCard}")
     public ResponseEntity<Object> deleteUserById(@PathVariable Long idCard){
         return iUserServiceInterface.DeleteUserById(idCard);
