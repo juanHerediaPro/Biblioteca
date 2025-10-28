6e31c0148bba5c63f0554b62b170cf13c620d9ab
juanherediapro <juanherediau@gmail.com>
2025-10-28 09:06:40 -0500

    arreglo controllers, testeo

--- Biblioteca/src/main/java/com/biblioteca/Biblioteca/Controllers/UserController.java
+++ Biblioteca/src/main/java/com/biblioteca/Biblioteca/Controllers/UserController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.biblioteca.Biblioteca.Entities.User;
 import com.biblioteca.Biblioteca.ServiceInterfaces.IUserServiceInterface;
 
 @RestController
-@RequestMapping("/users")
+// @RequestMapping("/users")
 public class UserController {
 
     private final IUserServiceInterface iUserServiceInterface;
@@ -22,27 +21,22 @@ public class UserController {
         this.iUserServiceInterface = iUserServiceInterface;
     }
 
-    @PostMapping("/create")
+    @PostMapping("/users/create")
     public ResponseEntity<Object> createUser(@RequestBody User user){
         return iUserServiceInterface.createUser(user);
     }
-    @GetMapping("/{idCard}")
+    @GetMapping("/users/{idCard}")
     public ResponseEntity<Object> getUserByidCard(@PathVariable Long idCard){
         return iUserServiceInterface.getUserByIdCard(idCard);
     }
-
-    @GetMapping({"", "/"}) // <--- AÑADE ESTA LÍNEA AQUÍ
+    
+    // Dejar /all si quieres mantenerlo, o eliminar el mapeo si solo quieres el nuevo
+    @GetMapping("/users/all")
     public ResponseEntity<Object> getAllUsers(){
         return iUserServiceInterface.getAllUsers();
     }
     
-    // Dejar /all si quieres mantenerlo, o eliminar el mapeo si solo quieres el nuevo
-    @GetMapping("/all") 
-    public ResponseEntity<Object> getAllUsersDeprecated(){
-        return iUserServiceInterface.getAllUsers();
-    }
-    
-    @DeleteMapping("/delete/{idCard}")
+    @DeleteMapping("/users/delete/{idCard}")
     public ResponseEntity<Object> deleteUserById(@PathVariable Long idCard){
         return iUserServiceInterface.DeleteUserById(idCard);
     }
